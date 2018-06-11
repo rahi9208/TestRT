83e43b3d56e8158fbd887e3358ea140164976b15
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-06-11 15:46:07 +0530

    adding translations

--- TestRT/list.js
+++ TestRT/list.js
@@ -1,5 +1,6 @@
 let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
+const tl = new AWS.Translate();
 exports.handler = function (event, context, callback) {
 
 	let response = {
@@ -20,12 +21,17 @@ exports.handler = function (event, context, callback) {
 			':queryType': queryType
 		},
 		FilterExpression: 'itemType = :queryType'
-	}, function (err, data) {
+	}, async function (err, data) {
 		if (data.Items) {
-			response.body = JSON.stringify(data.Items.map(item => {
+			response.body = JSON.stringify(await Promise.all(data.Items.map(async (item) => {
 				item.image = "https://s3.amazonaws.com/" + process.env["IMAGE_BUCKET"] + "/" + item.itemCode + ".jpg";
+				item.name = (await tl.translateText({
+					SourceLanguageCode: "en",
+					TargetLanguageCode: "fr",
+					Text: item.name
+				}).promise()).TranslatedText;
 				return item;
-			}));
+			})));
 			callback(null, response);
 		} else {
 			response.statusCode = 500;
