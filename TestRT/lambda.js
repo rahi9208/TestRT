79482e50fa3888e3203bb716566eb2e6a53aac36
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-06-11 14:45:34 +0530

    persisting to DB

---
+++ TestRT/lambda.js
@@ -0,0 +1,10 @@
+let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
+	ddb.put({
+		TableName: 'TestRT',
+		Item: { 'name': event.name, 'type': event.type, 'price': event.price }
+	}, function (err, data) {
+		callback(err, event);
+	});
+}
